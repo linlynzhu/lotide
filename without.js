c59eebd71cdd8c2ac2de22011944c8a82ff0e7e5
linzhu <lin.lyn.zhu@gmail.com>
2022-03-08 20:05:36 -0800

    find the same elements of two arrays

---
+++ without.js
@@ -0,0 +1,44 @@
+const eqArrays = function(array1, array2) {
+  let ans = false;
+  if (array2.length === array1.length) {
+    for (let i = 0; i <= array1.length - 1; i ++) {
+      if (array1[i] === array2[i]) {
+        ans = true;
+      } else {
+        ans = false;
+      }
+    }
+    console.log(ans);
+    return ans;
+  } else {
+    console.log(ans);
+    return ans;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log('🎉 the two arrays are the same');
+  } else {
+    console.log('⛈ why you need to know they are the same, they are not');
+  }
+};
+
+const without = function(array1, array2) {
+  let array = [...array1];
+  for (let elm of array1) {
+    for (let elm1 of array2) {
+      if (elm === elm1 && array.indexOf(elm) !== -1) {
+        array.splice(array.indexOf(elm),1);
+      }
+    }
+  }
+  console.log(array);
+  return array;
+};
+
+
+let words = ["hello", "world", "lighthouse"];
+without(words, ["hello",'world','haha',2,4,'world']); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
