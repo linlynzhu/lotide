afa2ea3685d37a8cfd52d91c4ed57759ef05c978
linzhu <lin.lyn.zhu@gmail.com>
2022-03-10 13:59:09 -0800

    add test func

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,27 @@
+const eqArrays = function(array1, array2) {
+  let ans = false;
+  if (array2.length === array1.length) {
+    for (let i = 0; i <= array1.length - 1; i ++) {
+      if (array1[i] === array2[i]) {
+        ans = true;
+      } else {
+        ans = false;
+      }
+    }
+    return ans;
+  } else {
+    return ans;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log('🎉 the two arrays are the same');
+  } else {
+    console.log('⛈ why you need to know they are the same, they are not');
+  }
+};
+
 const takeUntil = function(array, determineFunc) {
   let results = [];
   for (let elm of array) {
@@ -20,3 +44,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(results1, [1,2,5,7,2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
