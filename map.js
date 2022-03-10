8e1596da55556b2099bf2f53d5a4051e8014e8c6
linzhu <lin.lyn.zhu@gmail.com>
2022-03-10 13:39:23 -0800

    the sequence // update the result so that return it

---
+++ map.js
@@ -0,0 +1,12 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  let results = [];
+  for (let elm of array) {
+    results.push(callback(elm));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
