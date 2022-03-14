727ce5c474a0579c5febaa6c3cf0ac3247f04178
linzhu <lin.lyn.zhu@gmail.com>
2022-03-14 14:06:27 -0700

    remove pasted test functions and create test files to contain the manual test cut from the original files with export and require to link together

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+// TEST CODE
+const assertEqual = require('../assertEqual');
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Bootcamp", "Bootcamp");
