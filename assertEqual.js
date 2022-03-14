967ac4baa0bd3889a0b46fdb4f093034988d90e6
linzhu <lin.lyn.zhu@gmail.com>
2022-03-14 14:09:42 -0700

    removed the pasted test functions and replaced with require; cut the test samples to corresponding test files

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Bootcamp", "Bootcamp");
+module.exports = assertEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ✅${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ❌${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function(array1, array2) {
   let ans = false;
@@ -27,12 +20,4 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-
-eqArrays([1, 2, 3], [1, 2, 3]);// => true
-eqArrays([1, 2, 3], [3, 2, 1]);// => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]);// => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]);// => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([], []), true);
+module.exports = eqArrays;
