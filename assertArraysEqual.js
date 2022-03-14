be628b4e827b6e7fc94e781164acfddc7587630a
linzhu <lin.lyn.zhu@gmail.com>
2022-03-14 14:23:27 -0700

    replaced eqArrays function with require; created test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-const eqArrays = function(array1, array2) {
-  let ans = false;
-  if (array2.length === array1.length) {
-    for (let i = 0; i <= array1.length - 1; i ++) {
-      if (array1[i] === array2[i]) {
-        ans = true;
-      } else {
-        ans = false;
-      }
-    }
-    return ans;
-  } else {
-    return ans;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -22,8 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
