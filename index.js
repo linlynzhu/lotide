b9ed287c1f30392d88c0fa8096cd5224782acae9
linzhu <lin.lyn.zhu@gmail.com>
2022-03-14 22:32:18 -0700

    runkit results not expected

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const map = require('./map');
-const letterPositions = require('./letterPositions');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
-const eqObjects = require('./eqObjects');
-const assertObjectsEqual = require('./assertObjectsEqual');
+// const countLetters = require('./countLetters');
+// const countOnly = require('./countOnly');
+// const findKey = require('./findKey');
+// const findKeyByValue = require('./findKeyByValue');
+// const map = require('./map');
+// const letterPositions = require('./letterPositions');
+// const takeUntil = require('./takeUntil');
+// const without = require('./without');
+// const eqObjects = require('./eqObjects');
+// const assertObjectsEqual = require('./assertObjectsEqual');
 
 module.exports = {
   head,
@@ -22,14 +22,15 @@ module.exports = {
   assertArraysEqual,
   assertEqual,
   eqArrays,
-  countLetters,
-  countOnly,
-  findKey,
-  findKeyByValue,
-  letterPositions,
-  map,
-  takeUntil,
-  without,
-  eqObjects,
-  assertObjectsEqual
 };
+
+//countLetters,
+// countOnly,
+// findKey,
+// findKeyByValue,
+// letterPositions,
+// map,
+// takeUntil,
+// without,
+// eqObjects,
+// assertObjectsEqual
